a66ff8990a0e7ce2783d52526d18279e542847f9
lionelritchie29 <lionelrtchieee@gmail.com>
2020-05-08 18:42:32 +0700

    successfully make get request to api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-// import "./scss/main.scss";
+//Sass
 import "./scss/main-section.scss";
 import "./scss/showcase.scss";
+import "./scss/footer.scss";
 
 // Components
 import "./components/main-card/main-card.js";
@@ -8,4 +9,5 @@ import "./components/main-card/main-card.js";
 // Main Function
 import main from "./script/main.js";
 
+// Run
 window.addEventListener("DOMContentLoaded", main);
--- src/script/main.js
+++ src/script/main.js
@@ -1,13 +1,24 @@
+const axios = require("axios");
 import "../components/showcase-list/showcase-list.js";
 
 function main() {
-  const data = [1, 2, 3, 4];
-
-  data.forEach((a) => console.log(a));
+  const baseUrl = "https://api.spoonacular.com";
+  const apiKey = "8c45e5cc1bd3429e889d99bb19382990";
   const showcaseListElement = document.querySelector("showcase-list");
-  console.log(showcaseListElement);
-  console.log("row");
-  showcaseListElement.showcaseItems = data;
+
+  // Sending GET request to API, result : array of show case recipes
+  // to be used in showcase section
+  (function () {
+    axios
+      .get(`${baseUrl}/recipes/random?apiKey=${apiKey}&number=8`)
+      .then((response) => passInitialResponse(response.data.recipes))
+      .catch((error) => console.log(error));
+  })();
+
+  // Function to call showcase-list component settter
+  const passInitialResponse = (showcaseItems) => {
+    showcaseListElement.showcaseItems = showcaseItems;
+  };
 }
 
 export default main;
